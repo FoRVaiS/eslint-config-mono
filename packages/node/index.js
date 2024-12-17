8b8bd92c4ab0ad9437d0638855727527a15159dc
Benjamin Chiang <chiangbenjamin991@gmail.com>
2024-12-17 13:28:49 -0800

    feat: upgrade to ESLint flat configs

--- packages/node/index.js
+++ packages/node/index.js
@@ -1,49 +1,59 @@
-module.exports = {
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
+const node = require('eslint-plugin-node');
+const globals = require('globals');
+
+module.exports = [
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            }
+        },
+        plugins: {
+            node,
+        },
+        rules: {
+            'node/callback-return': 'error',
+            'node/exports-style': ['error', 'module.exports'],
+            'node/file-extension-in-import': ['error', 'always'],
+            'node/global-require': 'off',
+            'node/handle-callback-err': 'error',
+            'node/no-callback-literal': 'error',
+            'node/no-deprecated-api': 'error',
+            'node/no-exports-assign': 'error',
+            'node/no-extraneous-import': 'error',
+            'node/no-extraneous-require': 'error',
+            'node/no-missing-import': 'error',
+            'node/no-missing-require': 'error',
+            'node/no-mixed-requires': 'off',
+            'node/no-new-require': 'error',
+            'node/no-path-concat': 'error',
+            'node/no-process-env': 'off',
+            'node/no-process-exit': 'error',
+            'node/no-restricted-import': 'off',
+            'node/no-restricted-require': 'off',
+            'node/no-sync': 'off',
+            'node/no-unpublished-bin': 'error',
+            'node/no-unpublished-import': 'error',
+            'node/no-unpublished-require': 'error',
+            'node/no-unsupported-features/es-builtins': 'error',
+            'node/no-unsupported-features/es-syntax': 'error',
+            'node/no-unsupported-features/node-builtins': 'error',
+            'node/prefer-global/buffer': ['error', 'always'],
+            'node/prefer-global/console': ['error', 'always'],
+            'node/prefer-global/process': ['error', 'always'],
+            'node/prefer-global/text-decoder': ['error', 'always'],
+            'node/prefer-global/text-encoder': ['error', 'always'],
+            'node/prefer-global/url': ['error', 'always'],
+            'node/prefer-global/url-search-params': ['error', 'always'],
+            'node/prefer-promises/dns': 'error',
+            'node/prefer-promises/fs': 'error',
+            'node/process-exit-as-throw': 'off',
+            'node/shebang': 'error',
+        }
     },
-    plugins: [
-        'eslint-plugin-node',
-    ],
-    rules: {
-        'no-console': 'off',
-        'node/callback-return': 'error',
-        'node/exports-style': ['error', 'module.exports'],
-        'node/file-extension-in-import': ['error', 'always'],
-        'node/global-require': 'off',
-        'node/handle-callback-err': 'error',
-        'node/no-callback-literal': 'error',
-        'node/no-deprecated-api': 'error',
-        'node/no-exports-assign': 'error',
-        'node/no-extraneous-import': 'error',
-        'node/no-extraneous-require': 'error',
-        'node/no-missing-import': 'error',
-        'node/no-missing-require': 'error',
-        'node/no-mixed-requires': 'off',
-        'node/no-new-require': 'error',
-        'node/no-path-concat': 'error',
-        'node/no-process-env': 'off',
-        'node/no-process-exit': 'error',
-        'node/no-restricted-import': 'off',
-        'node/no-restricted-require': 'off',
-        'node/no-sync': 'off',
-        'node/no-unpublished-bin': 'error',
-        'node/no-unpublished-import': 'error',
-        'node/no-unpublished-require': 'error',
-        'node/no-unsupported-features/es-builtins': 'error',
-        'node/no-unsupported-features/es-syntax': 'error',
-        'node/no-unsupported-features/node-builtins': 'error',
-        'node/prefer-global/buffer': ['error', 'always'],
-        'node/prefer-global/console': ['error', 'always'],
-        'node/prefer-global/process': ['error', 'always'],
-        'node/prefer-global/text-decoder': ['error', 'always'],
-        'node/prefer-global/text-encoder': ['error', 'always'],
-        'node/prefer-global/url': ['error', 'always'],
-        'node/prefer-global/url-search-params': ['error', 'always'],
-        'node/prefer-promises/dns': 'error',
-        'node/prefer-promises/fs': 'error',
-        'node/process-exit-as-throw': 'off',
-        'node/shebang': 'error',
-    },
-};
+    {
+        rules: {
+            'no-console': 'off'
+        }
+    }
+]
